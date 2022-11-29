83398b3cbf3c447834eefcff6d4a19339a8fc356
李凯平V548 <v-kaiping.li@wz-inc.com>
2022-11-29 10:30:08 +0800

    add dockerfile

--- src/main/java/com/qxwz/qle/QleApplication.java
+++ src/main/java/com/qxwz/qle/QleApplication.java
@@ -1,6 +1,5 @@
 package com.qxwz.qle;
 
-import com.qxwz.galaxy.api.util.AppEnv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QleApplication {
 
     public static void main(String[] args) {
-        AppEnv.init("qle");
         SpringApplication application = new SpringApplication(QleApplication.class);
         application.run(args);
     }
